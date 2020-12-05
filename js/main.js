3874f13111fcd8e8ff765c8082d14ee353616955
Konstantin2222 <pribilkosta@gmail.com>
2020-12-05 16:38:05 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -1,32 +1,53 @@
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  effect: 'ccoverflow',
-})
-var hotelSlider = new Swiper('.hotel-slider', {
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+$(document).ready(function (){
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    effect: 'ccoverflow',
+  })
+  var hotelSlider = new Swiper('.hotel-slider', {
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+  
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  })
+  
+  var menuButton = document.querySelector('.menu-button')
+  menuButton.addEventListener('click', function() {
+    console.log('Клик по кнопке меню')
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom__visible')
+  });
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
 
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-})
+  function openModal(){
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass('modal__overlay_visible')
+    modalDialog.addClass('modal__dialog_visible')
 
-var menuButton = document.querySelector('.menu-button')
-menuButton.addEventListener('click', function() {
-  console.log('Клик по кнопке меню')
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom__visible')
+  }
+  function closeModal(event){
+    event.preventDefault()
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay_visible')
+    modalDialog.removeClass('modal__dialog_visible')
+  }
 });
